bc55300f80ef1d2233f51a0d452c7c318d605174
Vlad Melnyk <vladmelnyk04@gmail.com>
2023-03-20 07:45:25 +0200

    refactoring BackButton.php to php 8.1

--- Block/Adminhtml/Form/BackButton.php
+++ Block/Adminhtml/Form/BackButton.php
@@ -22,10 +22,7 @@ use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
  */
 class BackButton extends GenericButton implements ButtonProviderInterface
 {
-    /**
-     * @return array
-     */
-    public function getButtonData()
+    public function getButtonData(): array
     {
         return [
             'label' => __('Back'),
@@ -35,12 +32,7 @@ class BackButton extends GenericButton implements ButtonProviderInterface
         ];
     }
 
-    /**
-     * Get URL for back (reset) button
-     *
-     * @return string
-     */
-    public function getBackUrl()
+    public function getBackUrl(): string
     {
         return $this->getUrl('*/*/');
     }
