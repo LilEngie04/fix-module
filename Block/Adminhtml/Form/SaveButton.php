35e61470f88a50f99a33c9ce98f2b46fea425962
Vlad Melnyk <vladmelnyk04@gmail.com>
2023-03-20 15:35:53 +0200

    refactoring SaveButton.php to php 8.1

--- Block/Adminhtml/Form/SaveButton.php
+++ Block/Adminhtml/Form/SaveButton.php
@@ -22,10 +22,7 @@ use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
  */
 class SaveButton extends GenericButton implements ButtonProviderInterface
 {
-    /**
-     * @return array
-     */
-    public function getButtonData()
+    public function getButtonData(): array
     {
         return [
             'label' => __('Save'),
